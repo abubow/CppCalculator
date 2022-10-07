6755549ef04e7d53a1cce905b68a81a12fbaf1ca
smainc <40357673+smainc@users.noreply.github.com>
2022-10-07 22:59:04 +0500

    oops

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import CalculatorAPI from './CalculatorAPI';
 function App() {
   return (
     <div className="App">
-      <Calculator calculatorAPI={new CalculatorAPI("https://124a-34-131-187-2.in.ngrok.io/")}/>
+      <Calculator calculatorAPI={new CalculatorAPI("https://124a-34-131-187-2.in.ngrok.io")}/>
       <div>
         Still under development
         by <a href="https://abuzar.netlify.app">Abuzar</a>
